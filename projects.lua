b33e78b7f91b503a42f759d6dd9cb572f9f38179
wyrover <wyrover@gmail.com>
2017-03-07 10:36:14 +0800

    + 3rdparty/cairo

---
+++ projects.lua
@@ -0,0 +1,23 @@
+includeexternal ("premake-vs-include.lua")
+
+workspace "cairo-examples"
+    language "C++"
+    location "build/%{_ACTION}/%{wks.name}"    
+    if _ACTION == "vs2015" then
+        toolset "v140_xp"
+    end
+
+    
+
+    group "cairo-examples"    
+
+            
+        create_wtl_project("test_wtl_cario", "src")
+            includedirs
+            {
+                "3rdparty/cairo"
+            }
+            links
+            {
+                "3rdparty/cairo/libcairo-2.lib"
+            }
